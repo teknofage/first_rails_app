ed03d6d8c3074778a6550350cf2eb8b899ef7d94
teknofage <teknofage@gmail.com>
2020-09-27 23:31:03 -0700

    meddlied with database

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200828103837) do
+ActiveRecord::Schema.define(version: 20200924202518) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "body"
   end
 
 end
